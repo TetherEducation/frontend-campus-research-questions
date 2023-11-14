ee26da1dd7f40e0d358499ec12da07cf82b6ff18
ehvidalp <ehvidalp@gmail.com>
2023-11-14 12:55:46 -0600

    Add vacancies filter to explorer view

--- src/views/GoToExplorerView.vue
+++ src/views/GoToExplorerView.vue
@@ -57,6 +57,7 @@ const getSrcIframeExplorer = () => {
     url.searchParams.append('radius', '2');
     url.searchParams.append('z', '14.5');
     url.searchParams.append('filters', `${showFilters.value}`);
+    url.searchParams.append('vacancies', 'true')
     for (const grade of (grades || [])) {
         url.searchParams.append('grade', grade.toString());
     }
